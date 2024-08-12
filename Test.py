01163a8f0fa01de6fbc8292f68c6e54a3a18fb03
Adriana Bottega <65979221+abottegam@users.noreply.github.com>
2024-08-12 12:14:18 -0400

    Hallo World

---
+++ Test.py
@@ -0,0 +1,11 @@
+inp = input("Input: ").lower()
+fnd = input("Word to find: ")
+
+start = -1
+for letter in fnd:
+    start = inp.find(letter, start+1)
+    if start == -1:
+        print("No!!")
+        break
+else:
+    print("Yes :)")
